39e2b1b71b31e4f01ee3c2ee6457a4cdb807d9c6
Hüseyin Akbaş <h4kbas@gmail.com>
2024-08-21 20:03:50 +0300

    Change the color order of delimiters to prevent red comes first

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -2,5 +2,19 @@ return {
   -- Rainbow Highlighting
   {
     "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+      vim.g.rainbow_delimiters = {
+
+        highlight = {
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterBlue',
+          'RainbowDelimiterOrange',
+          'RainbowDelimiterGreen',
+          'RainbowDelimiterViolet',
+          'RainbowDelimiterCyan',
+          'RainbowDelimiterRed',
+        }
+      }
+    end
   }
 }
