2ab3e3f46b365165022c7b2afd5cb23478b38b56
Hüseyin Akbaş <h4kbas@gmail.com>
2024-08-21 20:03:27 +0300

    Solved auto formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+      local conform = require("conform")
+      conform.setup({
+        format_on_save = {
+          -- These options will be passed to conform.format()
+          timeout_ms = 500,
+          lsp_format = "fallback",
+        },
+        formatters_by_ft = {
+          lua = { "stylua" },
+
+          python = { "black" },
+          rust = { "rustfmt", lsp_format = "fallback" },
+          -- Conform will run the first available formatter
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+          typescript = { "prettierd", "prettier", stop_after_first = true },
+          typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+          tsx = { "prettierd", "prettier", stop_after_first = true },
+          json = { "prettierd", "prettier", stop_after_first = true },
+          jsonc = { "prettierd", "prettier", stop_after_first = true },
+        },
+      })
+    end,
+  },
+}
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -51,13 +51,3 @@ end
 
 vim.opt.fillchars:append({ fold = " " })
 vim.opt.foldtext = 'v:lua.MyFoldText()'
-
--- Format on save
-vim.api.nvim_create_augroup('AutoFormatting', {})
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*',
-  group = 'AutoFormatting',
-  callback = function()
-    vim.lsp.buf.format({ async = true })
-  end,
-})
