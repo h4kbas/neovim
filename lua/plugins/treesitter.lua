5d74a947e146f70e0d95dbf6d0744a6bdf55610b
Hüseyin Akbaş <h4kbas@gmail.com>
2024-08-24 02:02:36 +0300

    Add treesitter incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,16 @@ return {
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
-        fold = { enable = true }
+        fold = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-e>",
+            node_incremental = "<C-e>",
+            scope_incremental = false,
+            node_decremental = "<C-s>",
+          },
+        },
       })
     end
   }
