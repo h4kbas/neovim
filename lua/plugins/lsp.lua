e0ec9e9386d3c2e813a6a073abbda5e3bcb9ffc8
Hüseyin Akbaş <h4kbas@gmail.com>
2024-08-24 02:03:05 +0300

    Use manual lsp and remove lsp zero

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,65 @@
+return {
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { 'hrsh7th/cmp-nvim-lsp' },
+    },
+    config = function()
+      local lsp = require("lspconfig")
+      local cap = require("cmp_nvim_lsp").default_capabilities()
+      local lsp_attach = function(args)
+        local opts = { buffer = args.buf }
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', builtin.lsp_definitions, opts)
+        vim.keymap.set('n', 'gi', builtin.lsp_implementations, opts)
+        vim.keymap.set('n', 'gr', builtin.lsp_references, opts)
+        vim.keymap.set('n', 'go', builtin.lsp_type_definitions, opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        -- vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+      end
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "LSP actions",
+        callback = lsp_attach,
+      })
+
+      lsp.tsserver.setup({
+        capabilities = cap,
+      })
+
+      lsp.lua_ls.setup({
+        capabilities = cap,
+      })
+
+      lsp.sqlls.setup({
+        capabilities = cap,
+      })
+
+      lsp.jsonls.setup({
+        capabilities = cap,
+        filetypes = { 'json', 'jsonc' },
+        settings = {}
+      })
+
+      lsp.yamlls.setup({
+        capabilities = cap,
+        settings = {}
+      })
+
+
+      lsp.rust_analyzer.setup {
+        settings = {
+          ['rust-analyzer'] = {
+            diagnostics = {
+              enable = false,
+            }
+          }
+        }
+      }
+    end
+  },
+}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,27 +5,4 @@ return {
       require("mason").setup({})
     end,
   },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require('mason').setup({})
-      require('mason-lspconfig').setup({
-        ensure_installed = { 'tsserver', 'rust_analyzer', 'lua_ls', 'jsonls', 'sqlls', 'bashls' },
-        handlers = {
-          function(server_name)
-            require("lspconfig")[server_name].setup({})
-          end,
-          tsserver = function()
-            require("lspconfig").tsserver.setup({})
-          end,
-
-          lua_ls = function()
-            require("lspconfig").lua_ls.setup({
-              settings = { Lua = { diagnostics = { globals = { "vim" } } } },
-            })
-          end,
-        }
-      })
-    end
-  },
 }
