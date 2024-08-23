9462cd71a5ea159776141d2da50100cc7ec5671f
Hüseyin Akbaş <h4kbas@gmail.com>
2024-08-24 02:04:02 +0300

    Optimise bindings using cmd; adjust some bindings and add new ones

--- lua/vim-bindings.lua
+++ lua/vim-bindings.lua
@@ -2,33 +2,33 @@ local map = vim.keymap.set
 
 vim.g.mapleader = " "
 
--- Navigate vim panes better
-map('n', '<c-k>', ':wincmd k<CR>')
-map('n', '<c-j>', ':wincmd j<CR>')
-map('n', '<c-h>', ':wincmd h<CR>')
-map('n', '<c-l>', ':wincmd l<CR>')
+-- Session
+map("n", "<leader>q", "<cmd>mksession! save.vim<CR>:wall|qa!<CR>")
 
-map('n', '<M-Up>', ':wincmd k<CR>')
-map('n', '<M-Down>', ':wincmd j<CR>')
-map('n', '<M-Left>', ':wincmd h<CR>')
-map('n', '<M-Right>', ':wincmd l<CR>')
+-- Navigation
+map('n', '<c-k>', '<cmd>wincmd k<CR>')
+map('n', '<c-j>', '<cmd>wincmd j<CR>')
+map('n', '<c-h>', '<cmd>wincmd h<CR>')
+map('n', '<c-l>', '<cmd>wincmd l<CR>')
 
--- Terminal
-map('t', '<C-w>h', "<C-\\><C-n><C-w>h", { silent = true })
+map('n', '<M-Up>', '<cmd>wincmd k<CR>')
+map('n', '<M-Down>', '<cmd>wincmd j<CR>')
+map('n', '<M-Left>', '<cmd>wincmd h<CR>')
+map('n', '<M-Right>', '<cmd>wincmd l<CR>')
 
-map("t", "<M-k>", ":wincmd k<CR>")
-map("t", "<M-j>", ":wincmd j<CR>")
-map("t", "<M-h>", ":wincmd h<CR>")
-map("t", "<M-l>", ":wincmd l<CR>")
+map("t", "<M-k>", "<cmd>wincmd k<CR>")
+map("t", "<M-j>", "<cmd>wincmd j<CR>")
+map("t", "<M-h>", "<cmd>wincmd h<CR>")
+map("t", "<M-l>", "<cmd>wincmd l<CR>")
 
-map("t", "<M-Up>", ":wincmd k<CR>")
-map("t", "<M-Down>", ":wincmd j<CR>")
-map("t", "<M-Left>", ":wincmd h<CR>")
-map("t", "<M-Right>", ":wincmd l<CR>")
+map("t", "<M-Up>", "<cmd>wincmd k<CR>")
+map("t", "<M-Down>", "<cmd>wincmd j<CR>")
+map("t", "<M-Left>", "<cmd>wincmd h<CR>")
+map("t", "<M-Right>", "<cmd>wincmd l<CR>")
 
 
 
-map('n', '<leader>h', ':nohlsearch<CR>')
+map('n', '<leader>h', '<cmd>nohlsearch<CR>')
 
 -- Save
 vim.keymap.set("n", "S", "s", { noremap = true })
@@ -38,19 +38,18 @@ vim.keymap.set(
   "<cmd>w|lua vim.defer_fn( function()vim.cmd('echom \"\"') end, 0)<CR>",
   { noremap = false }
 )
-vim.keymap.set("n", "<leader>S", "<cmd>mksession! save.vim<CR>:wall|qa!<CR>")
 
 -- Move selected lines up/down in visual mode
-map("x", "K", ":move '<-2<CR>gv=gv")
-map("x", "J", ":move '>+1<CR>gv=gv")
-map("x", "<S-Up>", ":move '<-2<CR>gv=gv")
-map("x", "<S-Down>", ":move '>+1<CR>gv=gv")
+-- map("x", "K", "<cmd>move '<-2<CR>gv=gv")
+-- map("x", "J", "<cmd>move '>+1<CR>gv=gv")
+map("x", "<S-Up>", "<cmd>move '<-2<CR>gv=gv")
+map("x", "<S-Down>", "<cmd>move '>+1<CR>gv=gv")
 
 -- Move line up down in normal
 map("n", "<S-Up>", "yyddkP")
 map("n", "<S-Down>", "yyddp")
-map("n", "K", "yyddkP")
-map("n", "J", "yyddp")
+-- map("n", "K", "yyddkP")
+-- map("n", "J", "yyddp")
 
 -- Copy line up/down in normal
 map("n", "<M-S-Up>", "yyP")
@@ -59,9 +58,19 @@ map("n", "<M-S-k>", "yyP")
 map("n", "<M-S-j>", "yyp")
 
 -- Buffers
-map('n', '<leader>bd', ':bd<CR>')
-map('n', '<leader>bw', ':bw<CR>')
-map('n', '<leader>bn', ':enew<CR>')
+map('n', '<leader>bd', '<cmd>bd!<CR>')
+map('n', '<leader>bw', '<cmd>bw<CR>')
+map('n', '<leader>bn', '<cmd>enew<CR>')
+
+-- Panes
+map('n', '<leader>s', '<cmd>split<CR>')
+map('n', '<leader>S', '<cmd>vsplit<CR>')
+
+map("n", "<leader>s<Left>", "<C-w>H", {})
+map("n", "<leader>s<Right>", "<C-w>L", {})
+map("n", "<leader>s<Up>", "<C-w>K", {})
+map("n", "<leader>s<Down>", "<C-w>J", {})
+
 
 map("n", "<C-d>", "15j", { noremap = true })
 map("n", "<C-f>", "15k", { noremap = true })
@@ -72,3 +81,7 @@ map("n", "N", "Nzzzv")
 
 -- center after G
 map({ "n", "v" }, "G", "Gzz")
+
+-- Terminal
+map("n", "<leader>tt", "<cmd>tabnew | te<CR>", { noremap = true })
+map("t", "<ESC><ESC>", "<C-\\><C-n>", { noremap = true })
