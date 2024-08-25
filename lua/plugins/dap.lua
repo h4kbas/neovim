e1d83d646ce289e14f1055829e97144f156258b1
Hüseyin Akbaş <h4kbas@gmail.com>
2024-08-25 20:50:11 +0300

    Dap added with support to use javascript/typescript etc

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,8 +1,53 @@
 return {
-  { "mfussenegger/nvim-dap" },
   {
     "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require('dap')
+      local dapui = require('dapui')
+
+      dapui.setup()
+
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
+    end
   },
-  { "mxsdev/nvim-dap-vscode-js", requires = { "mfussenegger/nvim-dap" } },
+  { "nvim-neotest/nvim-nio" },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+    end,
+    keys = require("config.bindings").Custom_dap_mappings,
+    dependencies = {
+      {
+        "microsoft/vscode-js-debug",
+        build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+        version = "1.*",
+      },
+      {
+        "mxsdev/nvim-dap-vscode-js",
+        config = function()
+          require("dap-vscode-js").setup({
+            debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"),
+            adapters = {
+              "chrome",
+              "node",
+              "pwa-node",
+              "pwa-chrome",
+              "pwa-msedge",
+              "pwa-extensionHost",
+              "node-terminal",
+            },
+
+          })
+        end,
+      },
+    },
+  },
 }
