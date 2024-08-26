a64cfa4568cb4fc084b889ea65f57d378ab35bbc
Hüseyin Akbaş <h4kbas@gmail.com>
2024-08-26 20:17:26 +0300

    Fix Folding

--- lua/config/folding.lua
+++ lua/config/folding.lua
@@ -1,9 +1,8 @@
 -- Folding
-o.foldlevel = 99
-o.foldlevelstart = 99
-o.foldclose = 'all'
-o.foldmethod = 'expr'
-o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 function _G.MyFoldText()
   return vim.fn.getline(vim.v.foldstart) .. ' ... ' .. vim.fn.getline(vim.v.foldend):gsub("^%s*", "")
 end
