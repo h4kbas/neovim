dfea92d0a6124ee4a7e0c3ea9aa9b33c3fa3095b
Hüseyin Akbaş <h4kbas@gmail.com>
2026-03-03 12:45:36 +0300

    Fix

--- lua/plugins/cursor.lua
+++ lua/plugins/cursor.lua
@@ -1,7 +1,7 @@
 return {
   {
     "h4kbas/cursor.nvim",
-    dir = "~/Desktop/projects/cursor.nvim", -- For local development
+    -- dir = "~/Desktop/projects/cursor.nvim", -- For local development
     config = function()
       require('cursor').setup({
         chat_width = 50,
--- lua/plugins/neosql.lua
+++ lua/plugins/neosql.lua
@@ -1,6 +1,6 @@
 return {
   "h4kbas/neosql.nvim",
-  dir = "~/Desktop/projects/neosql", -- For local development
+  -- dir = "~/Desktop/projects/neosql", -- For local development
   config = function()
     require('neosql').setup({
     })
